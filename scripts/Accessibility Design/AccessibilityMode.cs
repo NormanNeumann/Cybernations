fb93ba333a6245a6545bf656cdb87e7fd5ffdbe6
XinyuYU0416 <u6366429@anu.edu.au>
2026-03-31 16:16:02 +1100

    add another mode for colorblind accessibility design.

---
+++ scripts/Accessibility Design/AccessibilityMode.cs
@@ -0,0 +1,6 @@
+public enum AccessibilityMode
+{
+	Off,
+	GlobalFilter,
+	BoardRecolor
+}
--- scripts/main/board/MainUI.Board.cs
+++ scripts/main/board/MainUI.Board.cs
@@ -2,56 +2,67 @@ using Godot;
 
 public partial class MainUI
 {
-    private static readonly PackedScene StackScene = GD.Load<PackedScene>("res://scenes/stacks/Stack.tscn");
+	private static readonly PackedScene StackScene = GD.Load<PackedScene>("res://scenes/stacks/Stack.tscn");
 
-    private void BuildHexCluster(Node2D parent)
-    {
-        var clusterSize = GetHexClusterSize();
-        var boardArea = new Rect2(Shift(new Vector2(240, 48)), new Vector2(1040, 690));
+	private void BuildHexCluster(Node2D parent)
+{
+	_stackViews.Clear();
 
-        var cluster = new Node2D();
-        cluster.Position = new Vector2(
-            boardArea.Position.X + (boardArea.Size.X - clusterSize.X) * 0.5f,
-            boardArea.Position.Y + (boardArea.Size.Y - clusterSize.Y) * 0.5f
-        );
+	var clusterSize = GetHexClusterSize();
+	var boardArea = new Rect2(Shift(new Vector2(240, 48)), new Vector2(1040, 690));
 
-        foreach (var tile in _hexTiles)
-        {
-            var stackView = StackScene.Instantiate<StackView>();
-            stackView.Position = tile.Position;
-            stackView.Configure(
-                tile.Base == HexBase.Wilds ? StackView.StackBaseKind.Wilds : StackView.StackBaseKind.Wasted,
-                tile.Overlay switch
-                {
-                    OverlayType.Human => StackView.StackOverlayKind.Human,
-                    OverlayType.Tech => StackView.StackOverlayKind.Tech,
-                    _ => StackView.StackOverlayKind.None,
-                },
-                false
-            );
-            cluster.AddChild(stackView);
-        }
+	var cluster = new Node2D();
+	cluster.Position = new Vector2(
+		boardArea.Position.X + (boardArea.Size.X - clusterSize.X) * 0.5f,
+		boardArea.Position.Y + (boardArea.Size.Y - clusterSize.Y) * 0.5f
+	);
 
-        parent.AddChild(cluster);
-    }
+	foreach (var tile in _hexTiles)
+	{
+		var stackView = StackScene.Instantiate<StackView>();
+		stackView.Position = tile.Position;
 
-    private Vector2 GetHexClusterSize()
-    {
-        var outerSize = GetHexBounds(HexOuterSide);
-        var maxX = 0.0f;
-        var maxY = 0.0f;
+		var baseKind = tile.Base == HexBase.Wilds
+			? StackView.StackBaseKind.Wilds
+			: StackView.StackBaseKind.Wasted;
 
-        foreach (var tile in _hexTiles)
-        {
-            maxX = Mathf.Max(maxX, tile.Position.X + outerSize.X);
-            maxY = Mathf.Max(maxY, tile.Position.Y + outerSize.Y);
-        }
+		var overlayKind = tile.Overlay switch
+		{
+			OverlayType.Human => StackView.StackOverlayKind.Human,
+			OverlayType.Tech => StackView.StackOverlayKind.Tech,
+			_ => StackView.StackOverlayKind.None,
+		};
 
-        return new Vector2(maxX, maxY);
-    }
+		stackView.Configure(baseKind, overlayKind, false);
+		
+		stackView.SetMeta("base_type", tile.Base.ToString());
+		stackView.SetMeta("overlay_type", tile.Overlay.ToString());
+		
+		_stackViews.Add(stackView);
 
-    private static Vector2 GetHexBounds(float sideLength)
-    {
-        return new Vector2(sideLength * 2.0f, Mathf.Sqrt(3.0f) * sideLength);
-    }
+		cluster.AddChild(stackView);
+	}
+
+	parent.AddChild(cluster);
+}
+
+	private Vector2 GetHexClusterSize()
+	{
+		var outerSize = GetHexBounds(HexOuterSide);
+		var maxX = 0.0f;
+		var maxY = 0.0f;
+
+		foreach (var tile in _hexTiles)
+		{
+			maxX = Mathf.Max(maxX, tile.Position.X + outerSize.X);
+			maxY = Mathf.Max(maxY, tile.Position.Y + outerSize.Y);
+		}
+
+		return new Vector2(maxX, maxY);
+	}
+
+	private static Vector2 GetHexBounds(float sideLength)
+	{
+		return new Vector2(sideLength * 2.0f, Mathf.Sqrt(3.0f) * sideLength);
+	}
 }
